0959beab3778e39c83efce99450da927e1b46d51
Datta-wakale <dattawakale2005@gmail.com>
2025-11-10 18:55:47 +0530

    new

---
+++ JavaPrograms/src/com/hetrogenous_array/Student.java
@@ -0,0 +1,22 @@
+package com.hetrogenous_array;
+
+public class Student 
+{
+	private int id;
+	private String name;
+	private double marks;
+	
+	public Student(int id,String name,double marks)
+	{
+		this.id=id;
+		this.name=name;
+		this.marks=marks;
+	}
+
+
+	public String toString() 
+	{
+		return "Student [id=" + id + ", name=" + name + ", marks=" + marks + "]";
+	}
+	
+}
