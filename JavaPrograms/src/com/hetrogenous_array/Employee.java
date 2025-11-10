d9e649579663d57c7bdf8a63072d1460fd4c6e56
Datta-wakale <dattawakale2005@gmail.com>
2025-11-10 18:55:10 +0530

    new

---
+++ JavaPrograms/src/com/hetrogenous_array/Employee.java
@@ -0,0 +1,23 @@
+package com.hetrogenous_array;
+
+public class Employee 
+{
+	private int id;
+	private String name;
+	private double salary;
+	
+	
+	public Employee(int id,String name,double salary)
+	{
+		this.id=id;
+		this.name=name;
+		this.salary=salary;
+	}
+	
+	public String toString() 
+	{
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+	
+	
+}
